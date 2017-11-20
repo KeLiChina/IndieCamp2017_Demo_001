efaab10e43002306a3ee00869459e543afee8815
24967 <forestlii@outlook.com>
2017-11-21 03:41:25 +0800

    01

---
+++ Project/JumpGame/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : RoleBase {
+
+protected override void Update()
+    {
+		base.Update();
+		if (Input.GetKeyDown(KeyCode.A))
+		{
+			M_Dic = -1;
+			speed = 1;
+			Walk();
+		}
+		if (Input.GetKeyDown(KeyCode.D))
+		{
+			M_Dic = 1;
+			speed = 1;
+			Walk();
+		}
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			Jump();
+		}
+		if (Input.GetKeyUp(KeyCode.A)||Input.GetKeyUp(KeyCode.D))
+		{
+			speed = 0;
+		}
+
+    }
+}
